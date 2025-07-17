4a9b3a44f4fecbdc2d0780678e511a53e81a6458
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-07-17 18:26:30 +0600

    update

---
+++ src/components/Div.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Div = ({className}) => {
+  return (
+    <div className={`bg-box rounded-lg ${className}`}></div>
+  )
+}
+
+export default Div
--- src/layouts/Services.jsx
+++ src/layouts/Services.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import Container from '../components/Container'
 import Flex from '../components/Flex'
+import Div from '../components/Div'
 
 const Services = () => {
   return (
     <section>
       <Container className=''>
         <Flex className='gap-x-[30px] mt-[-151px]'>
-          <div className='bg-box w-[270px] h-[350px] rounded-lg mt-[28px]'></div>
-          <div className='bg-box w-[200px] h-[496px] rounded-lg'></div>
-          <div className='bg-box w-[340px] h-[326px] rounded-lg mt-[116px]'></div>
-          <div className='bg-box w-[270px] h-[482px] rounded-lg mt-[34px]'></div>
-
+          <Div className={`w-[270px] h-[350px] mt-[28px]`} />
+          <Div className={`w-[200px] h-[496px]`} />
+          <Div className={`w-[340px] h-[326px] mt-[116px]`} />
+          <Div className={`w-[270px] h-[482px] mt-[34px]`} />
         </Flex>
       </Container>
       <Container className='mt-[100px] mb-[100px]'>
         <Flex className='justify-between'>
-          <div className='text-center'><span className='font-open text-[68px] font-extrabold text-primary block'>53k</span><span>Happy Client</span></div>
+          <div className='text-center'><span className='font-open text-[68px] font-extrabold text-primary block'>53k</span><span className='font-open text-[68px] font-extrabold text-primary block'>Happy Client</span></div>
           <div className='text-center'><span className='font-open text-[68px] font-extrabold text-primary block'>10k</span><span>Project Done</span></div>
           <div className='text-center'><span className='font-open text-[68px] font-extrabold text-primary block'>120k</span><span>Gets Award</span></div>
           <div className='text-center'><span className='font-open text-[68px] font-extrabold text-primary block'>16k</span><span>Operated Years</span></div>
