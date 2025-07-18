22fa40e317766df7e5a6e084d321e01ff481d90c
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-07-18 22:48:20 +0600

    update cards

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Button = ({ text, className }) => {
+const Button = ({ text, className, type}) => {
   return (
-    <button className={`py-3 px-5 md:py-[14px] md:px-7 bg-primary rounded-[100px] text-base md:text-lg text-white font-semibold font-open border border-transparent hover:bg-transparent hover:text-white hover:border-primary duration-300 ${className}`}>{text}</button>
+    {type===undefined?
+    <button className={`py-3 px-5 md:py-[14px] md:px-7 bg-primary rounded-[100px] text-base md:text-lg text-white font-semibold font-open border border-transparent hover:bg-transparent hover:text-white hover:border-primary duration-300 ${className}`}>{text}</button>:
+
+    <button className={`py-3 px-5 md:py-[14px] md:px-7 rounded-[100px] text-base md:text-lg text-white font-semibold font-open ${className}`}>{text}</button>}
   )
 }
 
