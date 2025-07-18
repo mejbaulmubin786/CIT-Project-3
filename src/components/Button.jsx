b3cbc343820e23361f894abf6afa8673be9548b3
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-07-18 23:29:40 +0600

    update somethind card

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ text, className, type}) => {
     !type?
     <button className={`py-3 px-5 md:py-[14px] md:px-7 bg-primary rounded-[100px] text-base md:text-lg text-white font-semibold font-open border border-transparent hover:bg-transparent hover:text-white hover:border-primary duration-300 ${className}`}>{text}</button>:
 
-    <button className={`py-3 px-5 md:py-[14px] md:px-7 rounded-[100px] text-base md:text-lg text-thrid font-bold font-open relative after:absolute after:top-0 after:right-0 after:w-[30px] after:h-full after:bg-red ${className}`}>{text}</button>
+    <button className={`py-3 px-5 md:py-[14px] md:px-7 rounded-[100px] text-base md:text-lg text-thrid font-bold font-open relative z-1 after:content-[''] after:absolute after:top-0 after:z-0 after:right-0 after:w-[50px] after:h-full after:rounded-r-[100px] after:bg-primary ${className}`}>{text}</button>
   )
 }
 
