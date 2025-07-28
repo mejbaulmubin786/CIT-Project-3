714b75f880bd52388bae909503e85a18ac78ac51
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-07-28 19:10:55 +0600

    fontawesome added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'; // <-- সঠিক ইম্পোর্ট
+//<FontAwesomeIcon icon={byPrefixAndName.fas['angle-down']} /> <FontAwesomeIcon icon="fa-solid fa-angle-down" />
 
 const List = ({ text, className }) => {
   return (
-    <li className={`text-[#EFF4FA] text-lg font-semibold	font-open hover:text-[#1BBF00] duration-300 cursor-pointer list-none ${className}`}>{text}</li>
-  )
-}
+    <li className={`text-[#EFF4FA] text-lg font-semibold font-open hover:text-[#1BBF00] duration-300 cursor-pointer list-none ${className}`}>
+      {text}
+      {/* সঠিক সিনট্যাক্স: ইম্পোর্ট করা আইকনটি props হিসেবে পাস করুন */}
+      <FontAwesomeIcon icon={faAngleDown} />
+    </li>
+  );
+};
 
-export default List
+export default List;
